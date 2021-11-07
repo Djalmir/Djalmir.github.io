a6af6b52f1a3c360e6bb7b72673fcbbab647addb
Djalmir <djalmir_miodutzki@hotmail.com>
2021-11-07 20:16:09 -0300

    Melhorias no visual

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function copyLink(){
 	navigator.clipboard.writeText(document.querySelector('#cmLink').innerText)
 }
+
+console.log(`
+	TIP:\n
+	In this console you can't declare variables like this:\n
+	var foo = 123\n
+	Instead, you just type:\n
+	foo = 123
+`)
