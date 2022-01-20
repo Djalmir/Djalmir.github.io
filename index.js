0f798f20c1fb01b54ae3db3b72dd599866206173
Djalmir <djalmir_miodutzki@hotmail.com>
2022-01-20 00:30:25 -0300

    bg

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+function move(e) {
+	mouseSeeker.style.top = (e.touches ? e.touches[e.touches.length - 1].clientY : e.clientY) - 50 + window.scrollY + 'px'
+	mouseSeeker.style.left = (e.touches ? e.touches[e.touches.length - 1].clientX : e.clientX) + window.scrollX + 'px'
+	e.preventDefault()
+}
+document.onmousemove = move
+document.ontouchmove = move
+
 function copyLink() {
 	navigator.clipboard.writeText(document.querySelector('#cmLink').innerText)
 	let msg = document.querySelector('#my-message')
-	msg.showMessage('Tag copied!',"Now you're just a CTRL + V away from seeing your project logs on your phone! 😀")
+	msg.showMessage('Tag copied!', "Now you're just a CTRL + V away from seeing your project logs on your phone! 😀")
 }
 
 console.log(`
