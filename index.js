3ecf1619ce2f6b683dc416283ab297674cd23d33
Djalmir <djalmir_miodutzki@hotmail.com>
2022-02-02 20:39:51 -0300

    vanilla js particles background

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function move(e) {
-	mouseSeeker.style.top = (e.touches ? e.touches[e.touches.length - 1].clientY : e.clientY) - 50 + window.scrollY + 'px'
-	mouseSeeker.style.left = (e.touches ? e.touches[e.touches.length - 1].clientX : e.clientX) + window.scrollX + 'px'
-	e.preventDefault()
-}
-document.onmousemove = move
-document.ontouchmove = move
+// function move(e) {
+// 	mouseSeeker.style.top = (e.touches ? e.touches[e.touches.length - 1].clientY : e.clientY) - 50 + window.scrollY + 'px'
+// 	mouseSeeker.style.left = (e.touches ? e.touches[e.touches.length - 1].clientX : e.clientX) + window.scrollX + 'px'
+// 	e.preventDefault()
+// }
+// document.onmousemove = move
+// document.ontouchmove = move
 
 function copyLink() {
 	navigator.clipboard.writeText(document.querySelector('#cmLink').innerText)
@@ -19,3 +19,140 @@ console.log(`
 	Instead, you just type:\n
 	foo = 123
 `)
+
+const canvas = document.querySelector('#canvas1')
+const c = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let particlesArray
+
+let mouse = {
+	x: undefined,
+	y: undefined,
+	radius: (canvas.height / 80) * (canvas.width / 80)
+}
+if (mouse.radius > 150)
+	mouse.radius = 150
+
+window.addEventListener('mousemove', (e) => {
+	mouse.x = e.x
+	mouse.y = e.y
+})
+window.addEventListener('touchmove', (e)=>{
+	mouse.x = e.touches[e.touches.length-1].clientX
+	mouse.y = e.touches[e.touches.length-1].clientY
+})
+
+class Particle {
+	constructor(x, y, directionX, directionY, size, color) {
+		this.x = x
+		this.y = y
+		this.directionX = directionX
+		this.directionY = directionY
+		this.size = size
+		this.color = color
+	}
+
+	draw() {
+		c.beginPath()
+		c.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
+		c.fillStyle = this.color
+		c.fill()
+	}
+
+	update() {
+		if (this.x > canvas.width || this.x < 0) {
+			this.directionX = -this.directionX
+		}
+		if (this.y > canvas.height || this.y < 0) {
+			this.directionY = -this.directionY
+		}
+
+		let dx = mouse.x - this.x
+		let dy = mouse.y - this.y
+		let distance = Math.sqrt(dx * dx + dy * dy)
+		if (distance < mouse.radius + this.size) {
+			if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+				this.x += 10
+			}
+			if (mouse.x > this.x && this.x > this.size * 10) {
+				this.x -= 10
+			}
+			if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+				this.y += 10
+			}
+			if (mouse.y > this.y && this.y > this.size * 10) {
+				this.y -= 10
+			}
+		}
+		this.x += this.directionX
+		this.y += this.directionY
+		this.draw()
+	}
+}
+
+function init() {
+	particlesArray = []
+	let numberOfParticles = (canvas.height * canvas.width) / 2000
+	if (numberOfParticles > 200)
+		numberOfParticles = 200
+	for (let i = 0; i < numberOfParticles; i++) {
+		let size = (Math.random() * 2) + 1
+		// let size = 1
+		let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2)
+		let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2)
+		let directionX = (Math.random() * 1) - .5
+		let directionY = (Math.random() * 1) - .5
+		let color = 'rgb(0, 153, 255)'
+
+		particlesArray.push(new Particle(x, y, directionX, directionY, size, color))
+	}
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+	c.clearRect(0, 0, innerWidth, innerHeight)
+
+	for (let i = 0; i < particlesArray.length; i++) {
+		particlesArray[i].update()
+	}
+
+	connect()
+}
+
+function connect() {
+	let opacityValue = 1
+	for (let a = 0; a < particlesArray.length; a++) {
+		for (let b = a; b < particlesArray.length; b++) {
+			let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x)) +
+				((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y))
+			if (distance < (canvas.width / 7) * (canvas.height / 7)) {
+				opacityValue = 1 - (distance / 10000)
+				c.strokeStyle = `rgba(0,51,102,${ opacityValue })`
+				c.lineWidth = 1
+				c.beginPath()
+				c.moveTo(particlesArray[a].x, particlesArray[a].y)
+				c.lineTo(particlesArray[b].x, particlesArray[b].y)
+				c.stroke()
+			}
+		}
+	}
+}
+
+window.addEventListener('resize', () => {
+	canvas.width = innerWidth
+	canvas.height = innerHeight
+	mouse.radius = ((canvas.height / 80) * (canvas.width / 80))
+	if (mouse.radius > 150)
+		mouse.radius = 150
+	init()
+})
+
+window.addEventListener('mouseout', () => {
+	mouse.x = undefined
+	mouse.y = undefined
+})
+
+init()
+animate()
